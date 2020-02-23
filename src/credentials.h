a2c5e70f9605437d1e344ce1734701b10bd40a7d
Vinicius Maciel <viniciusfre@gmail.com>
2020-02-23 08:35:00 -0300

    Removed credentials

--- src/credentials.h
+++ src/credentials.h
@@ -1,5 +1,5 @@
 
-#define CLIID       "674_mooSCsL7wDYoC1bpsXkrXtKrXePDxDZzoJhcjOP5irXaNBqn7w"
-#define CLSECRET    "8wKZL2czQHah4aJ1CIaCksQ3RI2wiEFLBU4s5SuWlDTiuRk7MG"
-#define ACCTOKEN    "44Fg5rDG2bRnE3s5vz34Kh5B-tyDmps3_Bsa5Zl5His"
-#define ACCID       14591137
+#define CLIID       "xxxx"
+#define CLSECRET    "xxxx"
+#define ACCTOKEN    "xxxx"
+#define ACCID       0000
